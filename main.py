6a055127abb3a7af76ab1e528321e50f9157a41e
Ifayemi19 <genevieveamagbegnon@gmail.com>
2025-06-25 12:08:08 +0200

    feat(main): intégration de Prometheus pour le monitoring via prometheus-fastapi-instrumentator

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI
-from app.controllers.weather_controller import router as weather_router  # ← import du router
+from app.controllers.weather_controller import router as weather_router
 from dotenv import load_dotenv
+from prometheus_fastapi_instrumentator import Instrumentator
+
+# Charger les variables d’environnement
 load_dotenv()
 
-app = FastAPI()
+# Création de l'application FastAPI
+app = FastAPI(title="Weather API")
 
-# Inclusion du routeur avec le préfixe /weather
-app.include_router(weather_router, prefix="/weather")  # ← crucial
+# Ajout du middleware Prometheus pour les métriques
+Instrumentator().instrument(app).expose(app)
 
+# Inclusion du routeur météo
+app.include_router(weather_router, prefix="/weather")
+
+# Endpoint de vérification de santé
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
